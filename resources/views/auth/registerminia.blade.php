6e36e0c52002873ca329beba591d4d4008900556
ProjectKangcutz <rizkyindranurbayu@gmail.com>
2023-12-06 10:28:31 +0700

    Membuat Login Page dng Google Recaptcha

--- resources/views/auth/registerminia.blade.php
+++ resources/views/auth/registerminia.blade.php
@@ -114,10 +114,6 @@
                                 </div>
                             </div>
 
-                            
-                            <div class=" text-center">
-                                <p class="text-gray-500 relative mb-5 dark:text-gray-100">© <script>document.write(new Date().getFullYear())</script> Minia . Crafted with <i class="mdi mdi-heart text-red-400"></i> by Themesbrand</p>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -189,17 +185,6 @@
             </div>
         </div>
     </div>
-
-
-    <!-- 
-        <div class="fixed ltr:right-5 rtl:left-5 bottom-10 flex flex-col gap-3 z-40 animate-bounce">
-            light-dark mode button 
-            <a href="javascript: void(0);" id="ltr-rtl" class="px-3.5 py-4 z-40 text-14 transition-all duration-300 ease-linear text-white bg-violet-500 hover:bg-violet-600 rounded-full font-medium" onclick="changeMode(event)">
-                <span class="ltr:hidden">LTR</span>
-                <span  class="rtl:hidden">RTL</span>
-            </a>  
-        </div> 
-    -->
 
     <script src="{{asset('minia/libs/@popperjs/core/umd/popper.min.js')}}"></script>
     <script src="{{asset('minia/libs/feather-icons/feather.min.js')}}"></script>
